546564b5408bdfb996f3de4610da4199c45532e3
bcaudillo <btcaudillo@gmail.com>
2022-11-22 20:09:23 -0700

    Updates

---
+++ precache-manifest.af5a617ae0a9002399ae78642d06e4e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca1bd0f68063b9203c78",
+    "url": "/static/css/main.b88cd803.chunk.css"
+  },
+  {
+    "revision": "ca1bd0f68063b9203c78",
+    "url": "/static/js/main.ca1bd0f6.chunk.js"
+  },
+  {
+    "revision": "76d904063a858c45df3d",
+    "url": "/static/js/1.76d90406.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1af7e05b70072dca96773631da83c723",
+    "url": "/index.html"
+  }
+];
